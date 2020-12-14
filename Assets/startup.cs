5a4c836db7008b0e396d586fc83a6f328c053b0a
DESKTOP-EFF58E2\sillydillys <hallc014@newschool.edu>
2020-12-14 04:48:13 -0500

    Final Merge

---
+++ Assets/startup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<runner>().enabled = false;
+        GameObject.Find("PrototypeWhite04x01 (1)").GetComponent<SpriteRenderer>().enabled = true;
+        GameObject.Find("Level Generator").GetComponent<recycle>().enabled = false;
+        GameObject.Find("Level Generator").GetComponent<scorepersecond>().enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J)) { 
+            GetComponent<runner>().enabled = true;
+        GameObject.Find("PrototypeWhite04x01 (1)").GetComponent<SpriteRenderer>().enabled = false;
+            GameObject.Find("Level Generator").GetComponent<recycle>().enabled = true;
+            GameObject.Find("Level Generator").GetComponent<scorepersecond>().enabled = true;
+
+        }
+    }
+}
